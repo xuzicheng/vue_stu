96b0a5ed25becba4121f4cb1b69c88ad237246b6
xuzicheng <32505601+xuzicheng@users.noreply.github.com>
2024-03-08 11:16:10 +0800

    更新

---
+++ src/components/Person.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <h2>地址：{{ address }}</h2>
+
+    <button @click="changename">修改名字</button>
+    <button @click="changeage">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+
+<script lang="ts" setup name="Person234">
+let name = '张三'  // 注意此时不是响应式
+let age = 12      // 注意此时不是响应式
+let tel = '123245'
+let address = '东莞市'
+
+//方法
+function changename() {
+  name = 'zhangsan'// 虽然改了，但这样修改并不是响应式的
+}
+
+function changeage() {
+  age += 1
+}
+
+function showTel() {
+  alert(tel)
+}
+</script>
+
+
+<style scoped>
+.person {
+  width: 750px;
+  height: 350px;
+  background-color: #79c9f1;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  margin: 0 5px;
+}
+</style>
+
+
---
+++ 备份/02_简单的效果/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changename">修改名字</button>
+    <button @click="changeage">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Person",
+  setup() {
+    //数据 原来是卸载 data 中，此时 name 啥的都不是响应式数据
+    let name = '张三'  // 注意此时不是响应式
+    let age = 12      // 注意此时不是响应式
+    let tel = '123245'
+
+    //方法
+    function changename() {
+      name = 'zhangsan'// 虽然改了，但这样修改并不是响应式的
+    }
+
+    function changeage() {
+      age += 1
+    }
+
+    function showTel() {
+      alert(tel)
+    }
+
+    return {name, age, changename, changeage, showTel}
+  }
+}
+</script>
+
+<style scoped>
+.person {
+  background-color: #79c9f1;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  margin: 0 5px;
+}
+</style>
