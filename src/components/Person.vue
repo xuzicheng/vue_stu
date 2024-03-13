c683b48a506a4cac9f47c05c7be1c2abb5e28bc2
xuzicheng <32505601+xuzicheng@users.noreply.github.com>
2024-03-13 20:52:25 +0800

    更新

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,29 +1,72 @@
 <template>
   <div class="person">
-    姓：<input type="text" v-model="firstname"><br>
-    名：<input type="text" v-model="lastname"><br>
-    全名：<span>{{ fullname }}</span><br>
+    <h2>情况 1：监视 ref 基本数据类型</h2>
+    <h2>当前求和为{{ sum }}</h2>
+    <button @click="changesum">点我sum+1</button>
+    <br>
+    <h2>情况 2：监视 ref 对象数据类型</h2>
+    <h2>姓名:{{ person.name }}</h2>
+    <h2>年龄:{{ person.age }}</h2>
+    <button @click="changename">修改名字</button>
+    <button @click="changeage">修改年龄</button>
+    <button @click="changePerson">修改整个人</button>
+
+
   </div>
 
 </template>
 
 
 <script lang="ts" setup name="Person234">
-import {computed, ref} from 'vue'
+import {ref, watch} from 'vue'
 
-let firstname = ref('张')
-let lastname = ref('三')
-
-let fullname = computed(() => {
-  return firstname.value.slice(0, 1).toUpperCase() + firstname.value.slice(1) + '-' + lastname.value
+let sum = ref(0)
+let person = ref({
+  name: '张三',
+  age: 18
 })
-</script>
 
+function changesum() {
+  sum.value += 1
+}
+
+//监视 情况 1：监视 ref 基本数据类型
+const stopwatch = watch(sum, (newValue, oldValue) => {
+  console.log('sum 变化了', newValue, oldValue)
+  // if (newValue>=10){
+  //   stopwatch()
+  // }
+})
+
+
+function changename() {
+  person.value.name += '~'
+}
+
+function changeage() {
+  person.value.age += 1
+}
+
+function changePerson() {
+  person.value = {name: '李四', age: 99}
+}
+
+/*监视 情况 2：监视 ref 对象数据类型,监视的是对象的地址值，若想监视对象内部的属性变化，需要手动开启深度监视,immediate立即给你 watch
+若修改的是ref定义的对象中的属性，newValue和oldValue都是新值，因为它们是同一个对象。
+若修改整个ref定义的对象，newValue是新值，oldValue是旧值，因为不是同一个对象了
+watch的第一个参数是：被监视的数据
+watch的第二个参数是：监视的回调
+watch的第三个参数是：配置对象（deep,immediate等等）
+ */
+watch(person, (newValue, oldValue) => {
+  console.log('person 变化了', newValue, oldValue)
+}, {deep: true, immediate: true})
+</script>
 
 <style scoped>
 .person {
-  width: 750px;
-  height: 350px;
+  width: 1150px;
+  height: 550px;
   background-color: #79c9f1;
   box-shadow: 0 0 10px;
   border-radius: 10px;
---
+++ 备份/11_compputed计算属性/components/Person.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="person">
+    姓：<input type="text" v-model="firstname"><br>
+    名：<input type="text" v-model="lastname"><br>
+    <button @click="changeFu11Name">改全名为li-si</button>
+    全名：<span>{{ fullname }}</span><br>
+  </div>
+
+</template>
+
+
+<script lang="ts" setup name="Person234">
+import {computed, ref} from 'vue'
+
+let firstname = ref('张')
+let lastname = ref('三')
+
+//这样定义的fullname 是计算属性，且是只读的
+// let fullname = computed(() => {
+//   return firstname.value.slice(0, 1).toUpperCase() + firstname.value.slice(1) + '-' + lastname.value
+// })
+
+
+//这样定义的fullname 是计算属性，可读可写
+let fullname = computed({
+  get() {
+    return firstname.value.slice(0, 1).toUpperCase() + firstname.value.slice(1) + '-' + lastname.value
+  },
+  set(val) {
+    const [str1, str2] = val.split('-')
+    firstname.value = str1
+    lastname.value = str2
+    console.log('set', val)
+  }
+})
+
+function changeFu11Name() {
+  fullname.value = 'li-si'
+}
+</script>
+
+<style scoped>
+.person {
+  width: 750px;
+  height: 350px;
+  background-color: #79c9f1;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  margin: 0 5px;
+}
+
+li {
+  font-size: 20px;
+}
+</style>
+
+
